3f926a674ce6a1758ab0cd4540a7bd5b8b61a220
Muhammad Ali <mr.muhammad.ali@live.com>
2017-07-20 02:36:16 +0500

    postgres url

--- src/index.js
+++ src/index.js
@@ -24,7 +24,29 @@ const app = feathers()
   .configure(socketio())
   .configure(services)
 
+var sequelize = require('sequelize-heroku').connect();
 
+if (sequelize)
+{
+  sequelize.authenticate().then( function() {
+    var config = sequelize.connectionManager.config;
+    console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+
+    sequelize.query('SELECT 1+1 as test').then( function(res) {
+
+      console.log('1+1='+res[0].test);
+
+    });
+
+  }).catch( function(err) {
+    var config = sequelize.connectionManager.config;
+    console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+  });
+}
+else
+{
+  console.log('No environnement variable found.');
+}
 
 
 
