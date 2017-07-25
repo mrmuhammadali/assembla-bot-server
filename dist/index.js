7d85ada9b979d1469c1e595e3cc60fec80613b51
Muhammad Ali <mr.muhammad.ali@live.com>
2017-07-25 21:28:42 +0500

    urlencoded

--- dist/index.js
+++ dist/index.js
@@ -51,6 +51,7 @@ app.get('/get-all', function (req, res) {
 app.post('/assembla-webhook', function (req, res) {
   var data = req.body;
   console.log("Webhook Request: ", data);
+  res.json({ success: true });
   bot.sendMessage(-219802955, JSON.stringify(data));
 });
 
