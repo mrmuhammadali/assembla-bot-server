54d32df36b558ed6ddaf8cb2027331251e2d1083
Muhammad Ali <mr.muhammad.ali@live.com>
2017-07-28 19:49:18 +0500

    adaptive card

--- dist/botOperations.js
+++ dist/botOperations.js
@@ -311,20 +311,14 @@ exports.BotOperations = function BotOperations() {
 
           console.log("isSkype: ", isSkype);
 
-          // session.send({
-          //   type: "message",
-          //   attachments: [
-          //     attachmentObj
-          //   ]
-          // });
+          session.send({
+            type: "message",
+            attachments: [attachmentObj]
+          });
 
           // builder.Prompts.choice(session, "Which region would you like sales for?", ["Green", "Red", "Blue"], builder.ListStyle.button);
           // session.send(msg);
 
-          var msg = new builder.Message(session);
-          msg.attachmentLayout(builder.AttachmentLayout.carousel);
-          msg.attachments([new builder.HeroCard(session).title("Classic White T-Shirt").subtitle("100% Soft and Luxurious Cotton").text("Price is $25 and carried in sizes (S, M, L, and XL)").images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')]).buttons([builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")]), new builder.HeroCard(session).title("Classic Gray T-Shirt").subtitle("100% Soft and Luxurious Cotton").text("Price is $25 and carried in sizes (S, M, L, and XL)").images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')]).buttons([builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")])]);
-          session.send(msg).endDialog();
         } else {
           var _opts = {
             reply_to_message_id: session.message_id,
